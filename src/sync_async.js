fd5bd023098f2f792fd970e56c818fa879a1229a
ibondarenko <ext-i.bondarenko@netbiscuits.com>
2013-06-01 12:13:14 +0300

    Added basic structure

---
+++ src/sync_async.js
@@ -0,0 +1,53 @@
+var SA = (function() {
+    'use strict';
+    
+    this.sync = function() {
+    
+    };
+
+    this.async = function() {
+
+    };
+
+    function ControlFlow() {
+        this.after = function(time, _) {
+
+        };
+        this.afterEach = function(_) {
+
+        };
+        this.complete = function(_) {
+
+        };
+    }
+
+    function Context() {
+        this.completed = [];
+        this.scheduled = [];
+        this.runnning = undefined;
+        this.destroy = function() {
+
+        };
+    }
+
+    function Promise() {
+        this.rejct = function(reason) {
+
+        };
+        this.resolve = function(result) {
+
+        };
+        this.terminate = function(reason) {
+
+        };
+        this.done = function(_) {
+
+        };
+        this.rejected = function(_) {
+
+        };
+        this.terminated = function(_) {
+
+        };
+    }
+})();
