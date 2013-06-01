4a8518fcf2ad952eef7fe5183a78541e3d939590
imapi <bondarenko.ivan.v@gmail.com>
2013-06-01 12:18:13 +0300

    Reformatting

--- src/sync_async.js
+++ src/sync_async.js
@@ -1,22 +1,22 @@
-var SA = (function() {
+var SA = (function () {
     'use strict';
-    
-    this.sync = function() {
-    
+
+    this.sync = function () {
+
     };
 
-    this.async = function() {
+    this.async = function () {
 
     };
 
     function ControlFlow() {
-        this.after = function(time, _) {
+        this.after = function (time, _) {
 
         };
-        this.afterEach = function(_) {
+        this.afterEach = function (_) {
 
         };
-        this.complete = function(_) {
+        this.complete = function (_) {
 
         };
     }
@@ -25,28 +25,28 @@ var SA = (function() {
         this.completed = [];
         this.scheduled = [];
         this.runnning = undefined;
-        this.destroy = function() {
+        this.destroy = function () {
 
         };
     }
 
     function Promise() {
-        this.rejct = function(reason) {
+        this.rejct = function (reason) {
 
         };
-        this.resolve = function(result) {
+        this.resolve = function (result) {
 
         };
-        this.terminate = function(reason) {
+        this.terminate = function (reason) {
 
         };
-        this.done = function(_) {
+        this.done = function (_) {
 
         };
-        this.rejected = function(_) {
+        this.rejected = function (_) {
 
         };
-        this.terminated = function(_) {
+        this.terminated = function (_) {
 
         };
     }
