fd871d31b503f7138ae9736178067fcf5af3d6c7
imapi <bondarenko.ivan.v@gmail.com>
2013-06-05 20:33:40 +0300

    Promise implementation, workflow changes

--- src/sync_async.js
+++ src/sync_async.js
@@ -1,53 +1,75 @@
 var SA = (function () {
     'use strict';
 
-    this.sync = function () {
+    //Compatibility
+    if (!('forEach' in Array.prototype)) {
+        Array.prototype.forEach = function (action, that) {
+            for (var i = 0, n = this.length; i < n; i++)
+                i in this && action.call(that, this[i], i, this);
+        };
+    }
 
+    this.sync = function () {
+        return new ControlFlow(new AsyncRunner(), new Context(arguments));
     };
 
     this.async = function () {
-
+        return new ControlFlow(new SyncRunner(), new Context(arguments));
     };
 
-    function ControlFlow() {
+    this.promise = function () {
+        return new Promise();
+    };
+
+    function Runner() {
+        this.start = function () {
+
+        }
+        this.tasks = undefined;
+    }
+
+    function SyncRunner() {
+        this.start = function () {
+
+        };
+        this.yield = undefined;
+    }
+
+    function AsyncRunner() {
+        this.start = function () {
+
+        };
+    }
+
+    function ControlFlow(runner, context) {
         this.after = function (time, _) {
 
         };
         this.afterEach = function (_) {
 
         };
-        this.complete = function (_) {
+        this.then = function (_) {
 
         };
     }
 
-    function Context() {
+    function Context(scheduled) {
         this.completed = [];
-        this.scheduled = [];
-        this.runnning = undefined;
+        this.scheduled = scheduled;
         this.destroy = function () {
 
         };
     }
 
     function Promise() {
-        this.rejct = function (reason) {
-
-        };
-        this.resolve = function (result) {
-
-        };
-        this.terminate = function (reason) {
-
-        };
-        this.done = function (_) {
-
-        };
-        this.rejected = function (_) {
-
-        };
-        this.terminated = function (_) {
-
-        };
+        var methods = ["reject", "resolve", "terminate"], then, done;
+        methods.forEach(function (element) {
+            this[element] = function () {
+                then ? then[element].call(undefined, arguments) : done = {name: element, args: arguments};
+            }
+        });
+        this.then = function (_) {
+            done ? _[done[name]].call(undefined, done.args) : then = _;
+        }
     }
 })();
